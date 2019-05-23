aa1879c244a4af8a48d63132366a1121410c8775
Kundan Goswami <kundangoswami18@gmail.com>
2019-05-23 20:50:02 +0530

    modified model

--- src/mdu_controller/src/controller.py
+++ src/mdu_controller/src/controller.py
@@ -33,21 +33,22 @@ class Mdu:
         #print(data.pose.pose)
         self.pose.x = round(self.pose.x, 4)
         self.pose.y = round(self.pose.y, 4)
-	#print(self.theta)
+	#print("Theta: " + str(self.theta))
 
     def euclidean_distance(self, goal_pose):
         """Euclidean distance between current pose and the goal."""
         return sqrt(pow((goal_pose.x - self.pose.x), 2) +
                     pow((goal_pose.y - self.pose.y), 2))
 
-    def linear_vel(self, goal_pose, constant=1.5):
-        
+    def linear_vel(self, goal_pose, constant=0.1):
+        #print("Distance: " + str(self.euclidean_distance(goal_pose)))
         return constant * self.euclidean_distance(goal_pose)
 
     def steering_angle(self, goal_pose):
         return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
 
-    def angular_vel(self, goal_pose, constant=6):
+    def angular_vel(self, goal_pose, constant=0.01):
+	#print("Angle: " + str(self.steering_angle(goal_pose)))
         return constant * (self.steering_angle(goal_pose) - self.theta)
 
     def move2goal(self):
@@ -66,18 +67,19 @@ class Mdu:
         while self.euclidean_distance(goal_pose) >= distance_tolerance:
 
             # Linear velocity in the x-axis.
-            vel_msg.linear.x = self.linear_vel(goal_pose,0.09)
+            vel_msg.linear.x = self.linear_vel(goal_pose)
             vel_msg.linear.y = 0
             vel_msg.linear.z = 0
 
             # Angular velocity in the z-axis.
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
-            vel_msg.angular.z = self.angular_vel(goal_pose,0.5)
+            vel_msg.angular.z = self.angular_vel(goal_pose)
 
             # Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
-
+	    #print("Linear Vel: " +  str(vel_msg.linear.x))
+            #print("Angular Vel: " +  str(vel_msg.angular.z))
             # Publish at the desired rate.
             self.rate.sleep()
 
